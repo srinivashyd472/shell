097f3d8fd42420697038267c8000f0a1ad0f79ec
srinivas <srinivashyd472@gmail.com>
2024-12-17 19:51:10 +0530

    p

---
+++ practice.sh
@@ -0,0 +1,16 @@
+a=10
+b=50
+c="how are you"
+
+# calling variables
+
+echo $a
+echo ${b}
+echo "${c}"
+
+#positional arguments
+
+echo "argument passing at position 0 is $0"
+echo "argument passing at position 1 is $1"
+echo "argument passing at position 2 is $1"
+echo "argument passing at position 3 is $2"
